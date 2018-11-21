4bc133db847af1724d885c386feae6fab78a7aa7
jinchongyang <43396261+jinchongyang@users.noreply.github.com>
2018-11-21 19:58:37 +0800

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ package cn.frt.demo2;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world111");
+		System.out.println("hello world222");
 	}
 }
